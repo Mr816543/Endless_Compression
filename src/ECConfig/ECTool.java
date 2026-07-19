efc95b1b328d07ffaa5d2b8bccc5e406a83cfacb
Mr816543 <1597195228@qq.com>
2026-07-19 21:46:33 +0800

    Initial commit

--- src/ECConfig/ECTool.java
+++ src/ECConfig/ECTool.java
@@ -228,8 +228,8 @@ public class ECTool {
         if (root.isVanilla()){
             if (root.uiIcon != null) {
                 child.uiIcon = child.fullIcon = mergeRegions(root.uiIcon, num);
-                //if(num == 1) Log.info(root.localizedName+"'s compression icon loaded successfully,times:"+times);
-            } else if (times <=100){
+                if(num == 1) Log.info(root.localizedName+"'s compression icon loaded successfully,times:"+times);
+            } else {
                 Core.app.post(() -> setIcon(root, child, num,times+1));
                 //if(num == 1) Log.info(root.localizedName+"'s compression icon failed to load,times:"+times);
             }
